dc798ddbda3640df87e117c043fab4f7d248dffb
UmedjonD <lazerr_94@mail.ru>
2018-12-10 17:09:32 +0400

    begin вывод в консаль

---
+++ src/main/java/Main.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) {
+        String query = "http://yandex.ru/maps";
+
+        HttpURLConnection connection = null;
+        try {
+            connection = (HttpURLConnection) new URL(query).openConnection();
+
+            connection.setRequestMethod("GET");
+            connection.setUseCaches(false);
+            connection.setConnectTimeout(500);
+            connection.setReadTimeout(500);
+
+            connection.connect();
+
+            StringBuilder ht = new StringBuilder();
+
+            if (HttpURLConnection.HTTP_OK == connection.getResponseCode()){
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                String line;
+                while ((line = in.readLine()) != null) {
+                    ht.append(line);
+                    ht.append("\n");
+                }
+                System.out.println(ht.toString());
+            }else {
+                System.out.println("fail: " + connection.getResponseCode() + ", " + connection.getResponseMessage());
+            }
+        } catch (Throwable cause) {
+            cause.printStackTrace();
+        }
+        finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
+}
