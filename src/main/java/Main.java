90871d8469f0e5c96658fc3038f7f078899ed366
UmedjonD <lazerr_94@mail.ru>
2018-12-14 12:04:01 +0400

    delete musor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,4 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class Main {
     public static void main(String[] args) throws IOException {
